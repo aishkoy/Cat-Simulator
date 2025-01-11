fbc51dcb2cf542dde5fe2a577407c763e2c73ba9
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-01-11 11:42:29 +0600

    feat: added createCat method for creating and adding Cat objects

--- src/models/CatManager.java
+++ src/models/CatManager.java
@@ -1,10 +1,14 @@
 package models;
 
+import services.IOManager;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class CatManager {
     private final List<Cat> cats;
+    private final static Random rand = new Random();
 
     public CatManager() {
         cats = new ArrayList<>();
@@ -16,5 +20,15 @@ public class CatManager {
 
     public List<Cat> getCats() {
         return cats;
+    }
+
+    public void createCat() {
+        String name = IOManager.getValidString("[a-zA-Zа-яА-Я]+", "Введите имя для кота: ");
+        int age = IOManager.getValidInt("1?[1-9]|1[0-8]", "Введите возраст для кота (1-18): ");
+        int hungerLevel = rand.nextInt(61) + 20;
+        int moodLevel = rand.nextInt(61) + 20;
+        int healthLevel = rand.nextInt(61) + 20;
+
+        cats.add(new Cat(name, age, hungerLevel, moodLevel, healthLevel));
     }
 }
