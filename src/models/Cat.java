08c0d0ea0c088cb50e129681382d1e45911933fb
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-01-11 12:02:51 +0600

    feat: added lifeStage field and determineCatLifeStage method to Cat class

--- src/models/Cat.java
+++ src/models/Cat.java
@@ -1,5 +1,7 @@
 package models;
 
+import enums.CatLifeStage;
+
 public class Cat {
     private final String name;
     private final int age;
@@ -8,6 +10,8 @@ public class Cat {
     private int healthLevel;
     private int averageLivingStandard;
 
+    private final transient CatLifeStage lifeStage;
+
     public Cat(String name, int age, int hungerLevel, int moodLevel, int healthLevel) {
         this.name = name;
         this.age = age;
@@ -15,9 +19,16 @@ public class Cat {
         this.moodLevel = moodLevel;
         this.healthLevel = healthLevel;
         this.averageLivingStandard = calculateAverageLivingStandard();
+        this.lifeStage = determineCatLifeStage();
     }
 
     private int calculateAverageLivingStandard() {
         return (int) Math.round((moodLevel + healthLevel + hungerLevel) / 3.0);
+    }
+
+    private CatLifeStage determineCatLifeStage () {
+        if( age < 6) return CatLifeStage.YOUNG;
+        else if (age < 11) return CatLifeStage.ADULT;
+        else return CatLifeStage.ELDER;
     }
 }
