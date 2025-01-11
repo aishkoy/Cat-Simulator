b2b8c2ef03cda6cde3422c84fbc985274ee684a5
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-01-11 15:20:27 +0600

    added method to display action menu

--- src/models/UserIO.java
+++ src/models/UserIO.java
@@ -13,10 +13,27 @@ public class UserIO {
     public void run(){
         System.out.println("Добро пожаловать в симулятор котов!");
         initializeFields();
+        while(true){
+            showActionMenu();
+        }
     }
 
     private void initializeFields(){
         JsonHandler.readJson(IOManager.getValidString("[a-zA-Z0-9._-]+", "Введите имя файла (пример: something.json): "));
         cm.setCats(JsonHandler.getCatList());
+    }
+
+    private void showActionMenu(){
+        System.out.println("""
+                            СИМУЛЯТОР КОТОВ
+                1. - Показать всех котов
+                2. - Завести нового кота
+                3. - Поиграть с котом
+                4. - Покормить кота
+                5. - Повести кота к ветеринару
+                6. - Отсортировать список котов
+                
+                0. - Выйти
+                """);
     }
 }
