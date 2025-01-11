8f8a6175ecfb0982470a58827468413645166d48
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-01-11 14:06:39 +0600

    feat: added method to print list of cats

--- src/models/CatManager.java
+++ src/models/CatManager.java
@@ -23,8 +23,8 @@ public class CatManager {
     }
 
     public void createCat() {
-        String name = IOManager.getValidString("[a-zA-Zа-яА-Я]+", "Введите имя для кота: ");
-        int age = IOManager.getValidInt("1?[1-9]|1[0-8]", "Введите возраст для кота (1-18): ");
+        String name = IOManager.getValidString("[a-zA-Zа-яА-Я]+", "Введите имя кота: ");
+        int age = IOManager.getValidInt("1?[1-9]|1[0-8]", "Введите возраст кота (1-18): ");
         int hungerLevel = rand.nextInt(61) + 20;
         int moodLevel = rand.nextInt(61) + 20;
         int healthLevel = rand.nextInt(61) + 20;
@@ -58,5 +58,22 @@ public class CatManager {
         System.out.println("""
                 Настал новый день!
                 Характеристики котов были изменены""");
+    }
+
+    public void printCats() {
+        if(cats.isEmpty()){
+            System.out.println("Список котов пуст! Добавьте котов!");
+            return;
+        }
+
+        System.out.println("╔══════╦═════════════════╦═════════╦══════════╦════════════╦═════════╦═════════════════╗");
+        System.out.println("║  №   ║       имя       ║ возраст ║ здоровье ║ настроение ║ сытость ║ средний уровень ║");
+        System.out.println("╠══════╬═════════════════╬═════════╬══════════╬════════════╬═════════╬═════════════════╣");
+        int catIndex = 0;
+        for(Cat cat : cats){
+            System.out.printf("║  %-3s ║ %-15s ║   %-3s   ║    %-3s   ║    %-3s     ║    %-3s  ║       %-8s  ║%n",
+                    (++catIndex),  cat.getName(), cat.getAge(), cat.getHealthLevel(), cat.getMoodLevel(), cat.getHungerLevel(), cat.getAverage());
+        }
+        System.out.println("╚══════╩═════════════════╩═════════╩══════════╩════════════╩═════════╩═════════════════╝");
     }
 }
